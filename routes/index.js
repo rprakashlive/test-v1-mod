655ca36d36ed81ea1014e3990f06da1abfff1fc6
Prakash <rprakashlive@gmail.com>
2020-04-03 17:38:19 +0530

    changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Socket check</h1>');
 });
 
 module.exports = router;
